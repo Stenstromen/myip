e8fb198dde2fb2caa30552983c9530686afcd8b8
Filip Stenström <hello@world.tld>
2022-06-15 10:55:07 +0200

    Updated controller to add \n to response, to fix curl zsh percentsign "feature"

--- controllers/mip.controller.js
+++ controllers/mip.controller.js
@@ -4,18 +4,30 @@ const forwardedFor = process.env.FORWARDEDFOR;
 
 function sendIp(req, res) {
   if (forwardedFor === "yes") {
-    res.status(200).end(req.headers["x-real-ip"]);
+    res
+      .status(200)
+      .setHeader("Content-Type", "text/plain")
+      .end(req.headers["x-real-ip"] + "\n");
   } else {
-    res.status(200).end(req.socket.remoteAddress);
+    res
+      .status(200)
+      .setHeader("Content-Type", "text/plain")
+      .end(req.socket.remoteAddress + "\n");
   }
 }
 
 function sendUserAgent(req, res) {
-  res.status(200).end(req.headers["user-agent"]);
+  res
+    .status(200)
+    .setHeader("Content-Type", "text/plain")
+    .end(req.headers["user-agent"] + "\n");
 }
 
 function sendReadme(req, res) {
-  res.status(200).end(printReadme);
+  res
+    .status(200)
+    .setHeader("Content-Type", "text/plain")
+    .end(printReadme + "\n");
 }
 
 module.exports = {
